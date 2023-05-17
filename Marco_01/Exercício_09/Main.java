54b4cb898002fe01aa53eca30b1bd8867948f503
PabloGuadagnin <pabloguadagnin.aluno@unipampa.edu.br>
2023-05-17 20:03:29 -0300

    2023 05 17 - 20:03
    Exercício 09

---
+++ Marco_01/Exercício_09/Main.java
@@ -0,0 +1,22 @@
+package Exercício_09;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Vetor vetor = new Vetor();
+        int vetorAleatorio[] = vetor.gerarNumerosAleatorios();
+        Random rand = new Random();
+        int valor = rand.nextInt(9999);
+
+        // bubble sort
+
+
+        // selection_sort
+
+
+        // insertion_sort
+
+    }
+}
